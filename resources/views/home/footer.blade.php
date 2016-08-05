d9d844b36c8f39a11bb02d86a7859e9d4e4ac8f0
mois3s <moi1432@gmail.com>
2016-08-04 22:06:31 -0400

    se corrige footer

--- resources/views/home/footer.blade.php
+++ resources/views/home/footer.blade.php
@@ -1,9 +1,29 @@
-<div class="pad-ding">
+<div class="pad-ding" id="footer">
 	<h1 align="center">Moisés Sepúlveda</h1>
 	<div class="mitad">
-		<i class="fa fa-phone-square en_linea icono_grande" style="font-size:2em" aria-hidden="true"></i>
-		<h2 class="en_linea" style="margin-bottom:20%;display:en_linea">+569 5357 2295</h2>
+		
+		<div class="en_linea">
+			<i class="fa fa-phone-square en_linea" style="font-size:2em"  aria-hidden="true"></i>
+			<h2 class="en_linea" style="margin-bottom:20%;display:en_linea">+569 5357 2295</h2>
+		</div>
+		
+		<div style="margin-left:20px" class="en_linea">
+			<div class="en_linea">
+				
+				<i class="fa fa-at" style="font-size:2em"  aria-hidden="true" ></i>
+				<h2 class="en_linea" style="margin-bottom:20%;display:en_linea;">moi1432@gmail.com</h2>
+			</div>
+			
+		</div>
+		
+		
 	</div>
-	<div class="mitad"></div>
+	<div class="mitad">
+		<div>
+			<i class="fa fa-github icono_social" style="font-size:2em" aria-hidden="true"></i>
+			<i class="fa fa-facebook-square icono_grande"  style="font-size:2em"aria-hidden="true"></i>
+			<i class="fa fa-twitter-square icono_grande"  style="font-size:2em"aria-hidden="true"></i>
+		</div>
+	</div>
 	<div class="clear"></div>
 </div>
--- resources/views/sections/nav.blade.php
+++ resources/views/sections/nav.blade.php
@@ -1,20 +1,4 @@
 
-<style>
-	/*#navegacion a {
-		text-decoration: none;
-		display: inline-block;
-		font-size: 20px;
-		border-bottom: 4px solid #009ED6;
-		margin-top:20px;
-		display: inline-block;
-		margin: 10px;
-		cursor:pointer;
-		color:#009ED6;
-		font-size: 20px;
-		transition: border-bottom 0.2s;
-	}*/
-
-</style>
 <header>
 	<section id="navegacion">
 		<a href="#contacto"><h3>Contacto</h3></a>
